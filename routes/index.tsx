067df84d17d3a3ce9165c329a747ae900cf59765
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-09 23:51:52 +0100

    add css reset to fresh site and make main div fullscreen for an app like look

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,16 +6,17 @@ export default function Home() {
     <>
       <Head>
         <title>Fresh App</title>
+        <link type="text/css" rel="stylesheet" href="/reset.css"></link>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
+      <div class="p-4 mx-auto w-full h-full bg-red-600">
         <img
           src="/logo.svg"
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
         <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
         </p>
         <Counter start={3} />
       </div>
