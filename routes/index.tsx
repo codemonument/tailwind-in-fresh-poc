1fb8f07538eb57a2251d5e5b33ab69fd2c8238c5
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-10 02:42:31 +0100

    remove fresh default components

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,16 +8,6 @@ export default function Home() {
     <Layout>
       <main class="p-4 w-full h-full ">
         <HeroBanner></HeroBanner>
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={3} />
       </main>
     </Layout>
   );
