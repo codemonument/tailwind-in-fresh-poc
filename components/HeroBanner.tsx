be4d35dbd436c036b4dec00383ea61fa925c418c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-10 02:37:38 +0100

    add Tailwind Heading

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,7 +1,11 @@
 export function HeroBanner() {
   return (
-    <div class="h-44 rounded-3xl bg-gradient-to-t from-[#5b1f65] to-[#c842dd] ">
-       
+    // <div class="h-44 rounded-3xl bg-gradient-to-t from-[#5b1f65] to-[#c842dd] font-sans flex flex-row items-center justify-center">
+    <div class="h-44 rounded-3xl bg-gradient-to-t from-blue-800 to-green-400 font-sans flex flex-row items-center justify-center border-2 border-opacity-50">
+      {/* <h1 class="text-9xl font-bold clip-text  bg-gradient-to-t from-[#eee] to-[#333]"> */}
+      <h1 class="text-9xl font-bold clip-text bg-white bg-opacity-80">
+        Tailwind in Fresh
+      </h1>
     </div>
   );
 }
